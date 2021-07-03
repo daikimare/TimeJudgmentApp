d95bd6f8be8c143c46154c2669f2bf00167aa09d
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-07-03 18:22:18 +0900

    type asertion setting

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 // DOM
-window.onload = () => {
-	const target = document.getElementById('target')
-	const start = document.getElementById('start')
-	const end = document.getElementById('end')
-	const judge = document.getElementById('judge')
-	const ans = document.getElementById('ans')
+const end = document.getElementById('end')
+const judge:any = document.getElementById('judge')
+const ans = document.getElementById('ans')
 
-	const main = () => {
-		console.log("app start!")
-		console.log("Get DOM")
-		console.log(target)
-		console.log(start)
-		console.log(end)
-		console.log(judge)
-		console.log(ans)
-	}
-	main()
+const main = () => {
+	console.log("app start!")
+	console.log("Get DOM")
+	// console.log(target)
+	// console.log(start)
+	console.log(end)
+	console.log(judge)
+	console.log(ans)
+}
+main()
+
+judge.onclick = function createDOM() {
+	const elementTarget: HTMLInputElement =<HTMLInputElement>document.getElementById('target')
+	const elementStart: HTMLInputElement =<HTMLInputElement>document.getElementById('start')
+	const target:number = Number(elementTarget.value)
+	const start:number = Number(elementStart.value)
+
+	console.log(target)
+	console.log(start)
 }
