16fe55e9a31e6b64b756a1844a1ef54d6ddfeec7
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-07-03 16:21:35 +0900

    set dom and getting dom in the ts file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
+// DOM
+window.onload = () => {
+	const target = document.getElementById('target')
+	const start = document.getElementById('start')
+	const end = document.getElementById('end')
+	const judge = document.getElementById('judge')
+	const ans = document.getElementById('ans')
 
-const main = () => {
-	console.log("app start")
+	const main = () => {
+		console.log("app start!")
+		console.log("Get DOM")
+		console.log(target)
+		console.log(start)
+		console.log(end)
+		console.log(judge)
+		console.log(ans)
+	}
+	main()
 }
