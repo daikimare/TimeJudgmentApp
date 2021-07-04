657060ff3f7dfdaabe09c99f1f95ff4e01be6261
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-07-05 03:38:44 +0900

    form reset

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // DOM
 const judge:any = document.getElementById('judge')
-const ans = document.getElementById('ans')
+const ans:string = String(document.getElementById('ans'))
 
 const main = () => {
 	console.log("app start!")
@@ -11,14 +11,34 @@ const main = () => {
 main()
 
 judge.onclick = () => {
-	const elementTarget:HTMLInputElement = <HTMLInputElement>document.getElementById('target')
-	const elementStart:HTMLInputElement = <HTMLInputElement>document.getElementById('start')
-	const elementEnd:HTMLInputElement = <HTMLInputElement>document.getElementById('end')
-	const target:number = Number(elementTarget.value)
-	const start:number = Number(elementStart.value)
-	const end:number = Number(elementEnd.value)
+	let elementTarget:HTMLInputElement = <HTMLInputElement>document.getElementById('target')
+	let elementStart:HTMLInputElement = <HTMLInputElement>document.getElementById('start')
+	let elementEnd:HTMLInputElement = <HTMLInputElement>document.getElementById('end')
+	let target:number = Number(elementTarget.value)
+	let start:number = Number(elementStart.value)
+	let end:number = Number(elementEnd.value)
 
 	console.log("target:",target)
 	console.log("start:",start)
 	console.log("end:",end)
+
+	if(target<0 || 24<target){
+		reset()
+		console.log("target:",target)
+		alert("判定する時間は0以上24以下の数値で入力してください")
+	}
+	if(start<0 || 24<start){
+		reset()
+		alert("判定範囲のスタート時刻は0以上24以下の数値で入力してください")
+	}
+	if(end<0 || 24<end){
+		reset()
+		alert("判定範囲の終了時刻は0以上24以下の数値で入力してください")
+	}
+
+}
+
+const reset = () => {
+	let form:HTMLFormElement = <HTMLFormElement>document.getElementById('inputForm')
+	form.reset()
 }
