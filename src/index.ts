b35be67b489ab990c4af38b098446275242eddb4
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-07-05 02:00:57 +0900

    fix scope

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,24 @@
 // DOM
-const end = document.getElementById('end')
 const judge:any = document.getElementById('judge')
 const ans = document.getElementById('ans')
 
 const main = () => {
 	console.log("app start!")
 	console.log("Get DOM")
-	// console.log(target)
-	// console.log(start)
-	console.log(end)
 	console.log(judge)
 	console.log(ans)
 }
 main()
 
-judge.onclick = function createDOM() {
-	const elementTarget: HTMLInputElement =<HTMLInputElement>document.getElementById('target')
-	const elementStart: HTMLInputElement =<HTMLInputElement>document.getElementById('start')
+judge.onclick = () => {
+	const elementTarget:HTMLInputElement = <HTMLInputElement>document.getElementById('target')
+	const elementStart:HTMLInputElement = <HTMLInputElement>document.getElementById('start')
+	const elementEnd:HTMLInputElement = <HTMLInputElement>document.getElementById('end')
 	const target:number = Number(elementTarget.value)
 	const start:number = Number(elementStart.value)
+	const end:number = Number(elementEnd.value)
 
-	console.log(target)
-	console.log(start)
+	console.log("target:",target)
+	console.log("start",start)
+	console.log("end",end)
 }
